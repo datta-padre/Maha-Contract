44795ce35181a05f1da5910cc7335ac4dbb38fa8
datta-padre <dattapadre357@gmail.com>
2026-08-18 14:34:56 +0530

    mysql

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql2')
 const util = require('util');
 
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',   
+//     password: 'root',
+//     database: 'buld'
+// });
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',   
-    password: 'root',
-    database: 'buld'
+    host: 'b697uso1rbxp394d9cwt-mysql.services.clever-cloud.com',
+    user: 'uge2ng8cqbgihba8',   
+    password: 'shuhauaOwMvEBzgyy4mHroot',
+    database: 'b697uso1rbxp394d9cwt'
 });
 
  const exe = util.promisify(connection.query).bind(connection);
